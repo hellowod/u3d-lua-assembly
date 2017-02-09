d71768edc1d709f17684d47c1f55bd981c33017b
abaojin <abaojin@gmail.com>
2017-02-09 13:59:20 +0800

    path update
    Signed-off-by: abaojin <abaojin@gmail.com>

--- tolua/Assets/ToLua/Editor/Custom/CustomSettings.cs
+++ tolua/Assets/ToLua/Editor/Custom/CustomSettings.cs
@@ -8,7 +8,7 @@ using System.Reflection;
 
 public static class CustomSettings
 {
-    public static string saveDir = Application.dataPath + "/Source/Generate/";    
+    public static string saveDir = Application.dataPath + "/Tolua/Source/Generate/";    
     public static string toluaBaseType = Application.dataPath + "/ToLua/BaseType/";    
 
     //导出时强制做为静态类的类型(注意customTypeList 还要添加这个类型才能导出)
